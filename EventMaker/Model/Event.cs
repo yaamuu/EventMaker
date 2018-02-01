da4e3454896d432338b41578a7a1f1dc457fc3b1
yaamuu <yaamu122@gmail.com>
2018-02-01 10:54:09 +0100

    ckasdsad

---
+++ EventMaker/Model/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMaker.Model
+{
+    class Event
+    {
+        private int Id;
+        private string Name;
+        private string Description;
+        private string Place;
+        private DateTime DateTime;
+
+
+
+    }
+}
