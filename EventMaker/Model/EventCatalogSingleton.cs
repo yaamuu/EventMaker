104403124eefaf87ca7a34589704655c5e00e52f
yaamuu <yaamu122@gmail.com>
2018-02-08 09:59:36 +0100

    sadsad

--- EventMaker/Model/EventCatalogSingleton.cs
+++ EventMaker/Model/EventCatalogSingleton.cs
@@ -16,9 +16,9 @@ namespace EventMaker.Model
         {
             EventList = new ObservableCollection<Event>();
 
-            EventList.Add(new Event(1, "Marcins Birthday", "He is turning 22 ", "Dieselvej 2", new DateTime(2018, 5, 9)));
-            EventList.Add(new Event(2, "Antonios Funeral", "Act sad", "Sandraekkerne 6", new DateTime(2018, 1, 7)));
-            EventList.Add(new Event(3, "Drinks with Jamshid", "Get smashed", "Mulagins Pub", new DateTime(2018, 2, 4)));
+            EventList.Add(new Event(1, "Marcins Birthday", "He is turning 22 ", "Dieselvej 2", new DateTime(2018, 5, 9,11,12,55)));
+            EventList.Add(new Event(2, "Antonios Funeral", "Act sad", "Sandraekkerne 6", new DateTime(2018, 1, 7,10,33,53)));
+            EventList.Add(new Event(3, "Drinks with Jamshid", "Get smashed", "Mulagins Pub", new DateTime(2018, 2, 4,7,12,33)));
         }
 
         public static EventCatalogSingleton GetInstance()
